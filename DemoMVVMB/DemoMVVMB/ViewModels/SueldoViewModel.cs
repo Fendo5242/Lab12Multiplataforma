47e1b7c206a49c688f06d8f525f48c02338874bd
Fendo5242 <fernando.sanchez@tecsup.edu.pe>
2023-12-02 21:23:39 -0500

    second commit

---
+++ DemoMVVMB/DemoMVVMB/ViewModels/SueldoViewModel.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace DemoMVVMB.ViewModels
+{
+    public class SueldoViewModel: ViewModelBase
+    {
+        double sueldo;
+        public double Sueldo
+        {
+            get { return sueldo; }
+            set { 
+                if(sueldo != value)
+                {
+                    sueldo= value;
+                    OnPropertyChanged();
+                } }
+        }
+
+        double gratificacion;
+        public double Gratificacion
+        {
+            get { return gratificacion; }
+            set
+            {
+                if (gratificacion != value)
+                {
+                    gratificacion = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        double sueldoneto;
+        public double SueldoNeto
+        {
+            get { return sueldoneto; }
+            set
+            {
+                if (sueldoneto != value)
+                {
+                    sueldoneto = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        double descuento;
+        public double Descuento
+        {
+            get { return descuento; }
+            set
+            {
+                if (descuento != value)
+                {
+                    descuento = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        double sueldodescuento;
+        public double SueldoDescuento
+        {
+            get { return sueldodescuento; }
+            set
+            {
+                if (sueldodescuento != value)
+                {
+                    sueldodescuento = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        double impuesto;
+        public double Impuesto
+        {
+            get { return impuesto; }
+            set
+            {
+                if (impuesto != value)
+                {
+                    impuesto = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public ICommand CalcularSueldoNeto { get; set; }
+        public ICommand CalcularDescuento { get; set; }
+        public ICommand CalcularImpuesto { get; set; }
+        public SueldoViewModel()
+        {
+            CalcularSueldoNeto = new Command(() => {
+                SueldoNeto = Sueldo + Gratificacion;
+            });
+            CalcularDescuento = new Command(() =>
+            {
+                SueldoDescuento = SueldoNeto - Descuento;
+            });
+            CalcularImpuesto = new Command(() =>
+            {
+                Impuesto = SueldoNeto * 0.18;
+
+            });
+        }
+    }
+}
---
+++ DemoMVVMB/DemoMVVMB/ViewModels/TaskViewModel.cs
@@ -0,0 +1,110 @@
+﻿using DemoMVVMB.Models;
+using DemoMVVMB.Views;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace DemoMVVMB.ViewModels
+{
+    public class TaskViewModel : ViewModelBase
+    {
+        string title;
+        public string Title
+        {
+            get { return title; }
+            set
+            {
+                if (title != value)
+                {
+                    title = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        string description;
+        public string Description
+        {
+            get { return description; }
+            set
+            {
+                if (description != value)
+                {
+                    description = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        bool status;
+        public bool Status
+        {
+            get { return status; }
+            set
+            {
+                if (status != value)
+                {
+                    status = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        ObservableCollection<TaskModel> tasks;
+        public ObservableCollection<TaskModel> Tasks
+        {
+            get { return tasks; }
+            set
+            {
+                if (tasks != value)
+                {
+                    tasks = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public ICommand Grabar { get; set; }
+
+        public ICommand Listar { get; set; }
+
+        public TaskViewModel()
+        {
+            // Inicializa la colección Tasks
+            Tasks = new ObservableCollection<TaskModel>();
+
+            Grabar = new Command(() => {
+                // Crea un nuevo objeto TaskModel
+                TaskModel task = new TaskModel();
+                task.Title = this.Title;
+                task.Description = this.Description;
+                task.Status = this.Status;
+
+                // Agrega el nuevo objeto a la colección
+                Tasks.Add(task);
+
+                // Limpia los campos después de grabar
+                Title = string.Empty;
+                Description = string.Empty;
+                Status = false;
+            });
+
+            Listar = new Command(() =>
+            {
+                Console.WriteLine("Listar button clicked"); // Asegúrate de que este mensaje aparezca en la consola
+                MostrarLista();
+            });
+        }
+
+        private void MostrarLista()
+        {
+            // Aquí puedes agregar lógica adicional si es necesario
+            // Por ejemplo, puedes imprimir en la consola o realizar alguna operación específica.
+            foreach (var task in Tasks)
+            {
+                Console.WriteLine($"Title: {task.Title}, Description: {task.Description}, Status: {task.Status}");
+            }
+        }
+    }
+}
